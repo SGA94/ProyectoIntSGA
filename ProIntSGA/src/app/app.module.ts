c323c1add6adcacadb9a80105e0f09d42d949f3b
SolizGabriel <soliz.gabriel@hotmail.com>
2023-01-13 23:59:10 -0300

    Agregamos el bloques Proyectos y Footer, modificamos titulos y detalles responsivos

--- ProIntSGA/src/app/app.module.ts
+++ ProIntSGA/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ExplabComponent } from './components/explab/explab.component';
 import { EstudiosComponent } from './components/estudios/estudios.component';
@@ -20,7 +19,6 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     HeaderComponent,
     AboutComponent,
     ProyectsComponent,
-    ContactComponent,
     FooterComponent,
     ExplabComponent,
     EstudiosComponent,
